7521db4dd4fbbd31a71ac92f2cb0309d2965d728
Iliass <iliassjabali@gmail.com>
2021-07-27 17:32:43 +0100

    more firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase'; // eslint-disable-next-line
+import firestore from 'firebase/firestore';
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDZg1LWImXV2a8UeGAAYmnBJMquex99hPs",
+  authDomain: "noteapplication-b3963.firebaseapp.com",
+  projectId: "noteapplication-b3963",
+  storageBucket: "noteapplication-b3963.appspot.com",
+  messagingSenderId: "1092605272194",
+  appId: "1:1092605272194:web:e7976a197a3c1096bd549b",
+  measurementId: "G-BTHRDL3E8B"
+});
+firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
